2b6b9cb4c7531f448daec498fa2363dfa57bab3e
Rishabh Yadav <yrishabh325@gmail.com>
2025-04-05 02:16:03 +0530

    A error

--- server/api/index.js
+++ server/api/index.js
@@ -23,7 +23,10 @@ const app = express();
 // Middleware
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(express.json()); // Parse JSON bodies
 app.use(cookieParser()); // Parse cookies
 
